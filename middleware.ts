ddbe0330ca612d56d6fac51e01bb84ed56d9584d
saurav <sauravjadhav2468@gmail.com>
2024-03-31 18:53:18 +0530

    middleware error

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,18 @@
-import { authMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
- 
-const protectedRoutes = createRouteMatcher([
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const protectedRoute = createRouteMatcher([
   '/',
   '/upcoming',
+  '/meeting(.*)',
   '/previous',
   '/recordings',
   '/personal-room',
-  '/meeting(.*)'
-])
+]);
 
-
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
-export default authMiddleware((auth, req) => {
-  if(protectedRoutes(req)) auth().protect();
+export default clerkMiddleware((auth, req) => {
+  if (protectedRoute(req)) auth().protect();
 });
- 
+
 export const config = {
-  matcher: [
-    // Exclude files with a "." followed by an extension, which are typically static files.
-    // Exclude files in the _next directory, which are Next.js internals.
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Re-include any files in the api or trpc folders that might have an extension
-    "/(api|trpc)(.*)"
-  ]
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
